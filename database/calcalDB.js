d9a3db4be97683f9832f93a76270236342ac6a01
Wachirapol Klinkasorn <88503947+wachirapol2002@users.noreply.github.com>
2023-10-18 02:31:14 +0700

    Create calcalDB.js

---
+++ database/calcalDB.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/compat";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAr1SVHEqT-7avHdevgdkZXcqxXs_3ngzI",
+    authDomain: "mobile-project-calcal.firebaseapp.com",
+    projectId: "mobile-project-calcal",
+    storageBucket: "mobile-project-calcal.appspot.com",
+    messagingSenderId: "132227373428",
+    appId: "1:132227373428:web:502038e10319ca054d3421",
+    measurementId: "G-P8JVEB9MKM"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
