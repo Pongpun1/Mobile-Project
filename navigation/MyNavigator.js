f189ac4a701d3affbda8d4c81d1a9437a49f8454
Pongpun <92890500+Pongpun1@users.noreply.github.com>
2023-10-19 01:03:40 +0700

    Update MyNavigator.js

--- navigation/MyNavigator.js
+++ navigation/MyNavigator.js
@@ -6,14 +6,17 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import MainScreen from "../screens/Main";
 import ProfileScreen from "../screens/ProfileScreen";
-
 import LoginScreen from "../screens/LoginScreen";
-
 import MenuScreen from "../screens/Menu";
 import BMIscreen from "../screens/BMI";
 import HistoryScreen from "../screens/History";
+import RegisterScreen from "../screens/RegisterScreen"; 
+import AccountScreen from "../screens/AccountScreen";
 
-import RegisterScreen from "../screens/RegisterScreen";
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { FontAwesome5 } from '@expo/vector-icons'; 
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -33,11 +36,51 @@ function Login(){
           options={{ headerShown: false }} name="เข้าสู่ระบบ" component={LoginScreen}/>
         <Stack.Screen name="สมัครสมาชิก" component={RegisterScreen} />
         <Stack.Screen name="ข้อมูลส่วนตัว" component={ProfileScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="แคลอรี่วันนี้" component={MainTabNavi}/>
+        <Stack.Screen options={{ headerShown: false }} name="แคลอรี่วันนี้" component={DrawerNavigator}/>
     </Stack.Navigator>
-
   )
 }
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator >
+        <Drawer.Screen  name="แคลอรี่วันนี้" component={MainTabNavi}
+        options={{
+          drawerLabel: "หน้าหลัก",
+        }} />
+        <Drawer.Screen name="แก้ไขข้อมูลส่วนตัว" component={ProfileNavi}
+        options={{
+          drawerLabel: "แก้ไขข้อมูลส่วนตัว",
+        }} />
+        <Drawer.Screen name="แก้ไขข้อมูลบัญชี" component={AccNavi}
+        options={{
+          drawerLabel: "แก้ไขข้อมูลบัญชี",
+        }} />
+        <Drawer.Screen name="Logout" component={Login}
+        options={{
+          drawerLabel: "ออกจากระบบ",
+        }} />
+      </Drawer.Navigator>
+  );
+}
+function ProfileNavi() {
+  return (
+    <Stack.Navigator 
+    screenOptions={{headerStyle: {backgroundColor: "#4a148c"}, headerTintColor: "white"}}>
+      <Stack.Screen options={{ headerShown: false }} name="ข้อมูลส่วนตัว" component={ProfileScreen} />
+    </Stack.Navigator>
+  )
+}
+
+function AccNavi() {
+  return (
+    <Stack.Navigator 
+    screenOptions={{headerStyle: {backgroundColor: "#4a148c"}, headerTintColor: "white"}}>
+      <Stack.Screen options={{ headerShown: false }} name="ข้อมูลบัญชีของคุณ" component={AccountScreen} />
+    </Stack.Navigator>
+  )
+}
+
+
 function MainTabNavi() {
   return (
     <Tab.Navigator screenOptions={{
@@ -45,106 +88,54 @@ function MainTabNavi() {
       headerTintColor: "black",
       headerTitleStyle: { fontWeight: "bold", fontSize: 25 },
     }}>
-      <Tab.Screen  name="แคลอรี่วันนี้" component={MainScreenNavi} />
-      <Tab.Screen  name="รายการแคลอรี่" component={MenuScreenNavi} />
-      <Tab.Screen  name="ดัชนีมวลกาย" component={BMIScreenNavi} />
-      <Tab.Screen  name="ประวัติ" component={HistoryScreenNavi}/>
+      <Tab.Screen 
+      name="แคลอรี่วันนี้" component={MainScreenNavi} options={{headerShown: false,tabBarIcon: () => {
+        return <AntDesign name="home" size={24} color="black" />}
+      }}/>
+      <Tab.Screen  name="รายการแคลอรี่" component={MenuScreenNavi} options={{headerShown: false,tabBarIcon: () => {
+        return <MaterialIcons name="restaurant-menu" size={24} color="black" />}
+      }}/>
+      <Tab.Screen  name="ดัชนีมวลกาย" component={BMIScreenNavi} options={{headerShown: false,tabBarIcon: () => {
+        return <FontAwesome5 name="weight" size={24} color="black" />}
+      }}/>
+      <Tab.Screen  name="ประวัติ" component={HistoryScreenNavi} options={{headerShown: false,tabBarIcon: () => {
+        return <AntDesign name="calendar" size={24} color="black" />}
+      }}/>
     </Tab.Navigator>
   )
 }
 function MainScreenNavi() {
   return (
     <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="หน้าหลัก" component={MainScreen}/>
+        <Stack.Screen name="หน้าหลัก" component={MainScreen}/>
     </Stack.Navigator>
   )
 }
 function MenuScreenNavi(){
   return(
     <Stack.Navigator>
-      <Stack.Screen options={{ headerShown: false }} name="รายการแคลอรี่" component={MenuScreen}/>
+      <Stack.Screen name="รายการแคลอรี่" component={MenuScreen}/>
     </Stack.Navigator>
   )
 }
 function BMIScreenNavi(){
   return(
     <Stack.Navigator>
-      <Stack.Screen options={{ headerShown: false }} name="ดัชนีมวลกาย" component={BMIscreen}/>
+      <Stack.Screen name="ดัชนีมวลกาย" component={BMIscreen}/>
     </Stack.Navigator>
   )
 }
 function HistoryScreenNavi(){
   return(
     <Stack.Navigator>
-      <Stack.Screen options={{ headerShown: false }} name="ประวัติ" component={HistoryScreen}/>
+      <Stack.Screen name="ประวัติ" component={HistoryScreen}/>
     </Stack.Navigator>
   )
 }
 
-// function ProfileNavi() {
-//   return (
-//     <Stack.Navigator 
-//     screenOptions={{headerStyle: {backgroundColor: "#4a148c"}, headerTintColor: "white"}}>
-//       <Stack.Screen name="ข้อมูลส่วนตัว" component={ProfileScreen} />
-//     </Stack.Navigator>
-//   )
-// }
 
-// function AccNavi() {
-//   return (
-//     <Stack.Navigator 
-//     screenOptions={{headerStyle: {backgroundColor: "#4a148c"}, headerTintColor: "white"}}>
-//       <Stack.Screen name="ข้อมูลบัญชีของคุณ" component={AccountScreen} />
-//     </Stack.Navigator>
-//   )
-// }
 
-// function LogoutNavi() {
-//   return (
-//     <Stack.Navigator 
-//     screenOptions={{headerStyle: {backgroundColor: "#4a148c"}, headerTintColor: "white"}}>
-//       <Stack.Screen name="เข้าสู่ระบบ" component={LoginScreen} />
-//     </Stack.Navigator>
-//   )
-// }
 
-// function MainNavigator() {
-//   return (
-//     <Drawer.Navigator>
-//         <Drawer.Screen name="Main" component={MainTabNavi}
-//         options={{
-//           drawerLabel: "หน้าหลัก",
-//         }} />
-//         <Drawer.Screen name="EditProfile" component={ProfileNavi}
-//         options={{
-//           drawerLabel: "แก้ไขข้อมูลส่วนตัว",
-//         }} />
-//         <Drawer.Screen name="EditAcc" component={AccNavi}
-//         options={{
-//           drawerLabel: "แก้ไขข้อมูลบัญชี",
-//         }} />
-//         <Drawer.Screen name="Logout" component={LogoutNavi}
-//         options={{
-//           drawerLabel: "ออกจากระบบ",
-//         }} />
-//       </Drawer.Navigator>
-
-//     <Stack.Navigator
-//         initialRouteName="LoginScreen"
-//         screenOptions={{
-//           headerStyle: { backgroundColor: "#71B2FF" },
-//           headerTintColor: "black",
-//           headerTitleStyle: { fontWeight: "bold", fontSize: 25 },
-//         }}
-//       >
-//         <Stack.Screen
-//           options={{ headerShown: false }} name="เข้าสู่ระบบ" component={LoginScreen}/>
-//         <Stack.Screen name="สมัครสมาชิก" component={RegisterScreen} />
-//         <Stack.Screen name="ข้อมูลส่วนตัว" component={ProfileScreen} />
-//         <Stack.Screen name="แคลอรี่วันนี้" component={MainScreen }/>
-//     </Stack.Navigator>
-//   );
-// }
 
 export default function MyNavigator() {
   return (
