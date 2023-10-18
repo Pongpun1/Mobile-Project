5c958abb6055b100f71fee691f01101f6c88aa64
Pongpun <92890500+Pongpun1@users.noreply.github.com>
2023-10-18 19:50:50 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MyNavigator from "./navigation/MyNavigator";
 
-import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import ProfileScreen from './screens/ProfileScreen';
+const Stack = createNativeStackNavigator();
 
-const stack = createNativeStackNavigator();
 export default function App() {
- return (
-      <NavigationContainer>
-        <stack.Navigator initialRouteName='LoginScreen'
-        screenOptions={{headerStyle: {backgroundColor: "#71B2FF"}, headerTintColor: "black", 
-        headerTitleStyle: {fontWeight: 'bold', fontSize: 25}}}>
-          <stack.Screen options={{headerShown: false}}  name='เข้าสู่ระบบ' component={LoginScreen}/>
-          <stack.Screen name='สมัครสมาชิก' component={RegisterScreen}/>
-          <stack.Screen name='ข้อมูลส่วนตัว' component={ProfileScreen}/>
-        </stack.Navigator>
-      </NavigationContainer>
- );
+  return (
+    <MyNavigator />
+    
+ // <NavigationContainer>
+    //   <Stack.Navigator
+    //     initialRouteName="LoginScreen"
+    //     screenOptions={{
+    //       headerStyle: { backgroundColor: "#71B2FF" },
+    //       headerTintColor: "black",
+    //       headerTitleStyle: { fontWeight: "bold", fontSize: 25 },
+    //     }}
+    //   >
+    //     <Stack.Screen
+    //       options={{ headerShown: false }}
+    //       name="เข้าสู่ระบบ"
+    //       component={LoginScreen}
+    //     />
+    //     <Stack.Screen name="สมัครสมาชิก" component={RegisterScreen} />
+    //     <Stack.Screen name="ข้อมูลส่วนตัว" component={ProfileScreen} />
+    //     <Stack.Screen name="แคลอรี่วันนี้" component={MainScreen} />
+    //     <Stack.Screen name="เพิ่มเมนูอาหาร" component={AddFoodScreen} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+  );
 }
 
 const styles = StyleSheet.create({
- screen: {
+  container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
- },
- screens: {
-  backgroundColor: '#D9D9D9'
-},
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
+
