5a2e9f430fbefe14bf5eb49ef43c50e0ac6121ad
PongpunSaengphon <pp_saeng@hotmail.com>
2023-10-25 23:37:08 +0700

    Main Update

--- screens/Main.js
+++ screens/Main.js
@@ -1,112 +1,70 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  TextInput,
-  TouchableOpacity,
-  Pressable,
-  Platform,
-  Alert
-} from "react-native";
-import firebase from '../database/calcalDB';
-import {Picker} from '@react-native-picker/picker';
-import { useSelector, useDispatch, connect } from "react-redux";
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { userData, clearData } from "../store/actions/userAction";
-import { createSelector } from 'reselect';
+  BarChart,
+  PieChart,
+} from "react-native-chart-kit";
 
+const MainScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.HeadText}>แคลอรี่ในวันนี้ของคุณ</Text>
+        <BarChart
+          data={{
+            labels: ["แคลอรี่ที่ควรได้รับ", "แคลอรี่ที่ได้รับ", "คงเหลือ"],
+            datasets: [
+              {
+                // ใส่ข้อมูลตรงนี้
+                data: [1500, 1200, 400],
+                colors: [
+                  () => '#48ACCB',
+                  () => '#72CB48',
+                  () => '#F99D49',
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get("window").width * 0.9}
+          height={250}
+          fromZero='true'
+          yAxisSuffix="kcal"
+          yAxisInterval={1}
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#9FC9FB",
+            backgroundGradientFromOpacity:0.6,
+            backgroundGradientTo: "#76B5FF",
+            backgroundGradientToOpacity:1,
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            barPercentage: 0.7,
+          }}
+          showBarTops={false}
+          withCustomBarColorFromData
+          style={{
+            marginVertical: 8,
+            borderRadius: 10,
+            
+            fontWeight: 'bold',
+          }}
+        />
+    </View>
+  );
+};
 
-const MainScreen = ({route, navigation}) =>{
-  const userKey = useSelector(state => state.account);
-  const test = () => {
-   
-    
-    // In your component
-    console.log(userKey);
-    // console.log(reduxState)
-  }
-  return(
-    <TouchableOpacity
-        style={styles.button}
-        onPress={() => test()}
-        >
-        <Text style={styles.buttonText}>บันทึก</Text>
-      </TouchableOpacity>
-  )
-}
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
     flex: 1,
+    alignItems: "center",
+    marginLeft: 50,
+    marginRight: 50,
   },
   HeadText: {
     fontWeight: "bold",
-    fontSize: 30,
-    marginTop: 50,
-    marginBottom: 10,
-  },
-  tdeeBox: {
-    justifyContent: 'center',
-    height: 60,
-    width: 350,
-    alignItems: "center",
-    backgroundColor: '#90C2FD',
-    borderRadius: 10,
-  },
-  tdeeText: {
-    fontWeight: "bold",
     fontSize: 25,
-  },
-  tdeeShow:{
-    fontWeight: "bold",
-    height: 40,
-    width: 80,
-    borderRadius: 10,
-    backgroundColor: "white",
-    fontSize: 20,
-    opacity: 1,
-    textAlign:'center'
-  },
-  input: {
-    height: 40,
-    width: 360,
-    borderRadius: 50,
-    borderWidth: 0,
-    padding: 10,
-    backgroundColor: "#D9D9D9",
-    fontSize: 15,
-    opacity: 1,
-  },
-  button: {
-    width: 100,
-    height: 40,
-    marginTop: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#90C2FD",
-    borderRadius: 100,
-  },
-  buttonText: {
-    color: "black",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  dropdown: {
-    width: 360,
-    height: 40,
-    backgroundColor: "#D9D9D9",
-    borderRadius: 100,
-    borderWidth: 0,
-  },
-  Text: {
-    fontWeight: "bold",
-    fontSize: 16,
-    marginTop: 10,
-    marginBottom: 6,
+    marginTop: 20,
   },
 });
 
 export default MainScreen;
-
