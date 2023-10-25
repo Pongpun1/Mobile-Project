96b9b4d707366f57c3a6319c66f4369b29239885
ZofearZ <64070203@it.kmitl.ac.th>
2023-10-26 03:41:51 +0700

    create_full_addActivity

--- screens/AddActivity.js
+++ screens/AddActivity.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
-const AccountScreen = ({navigation}) => {
+const AddActivity = ({ navigation }) => {
   return (
-    <>
-    Add Activity
-    </>
-  );
-}
+    <View style={styles.container}>
+      <Text style={styles.HeadText}>เพิ่มกิจกรรม</Text>
 
-export default AccountScreen;
+      <View style={styles.container2}>
+        <Text style={styles.Text}>ชื่อกิจกรรม ระยะเวลา 1 ชั่วโมง</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.Text}>แคลอรี่</Text>
+        <TextInput style={styles.input} />
+      </View>
+
+      <TouchableOpacity style={styles.roundButton1}>
+        <Text style={styles.buttonText}>เพิ่ม</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  container2: {
+    marginTop: 15,
+  },
+  HeadText: {
+    fontWeight: "bold",
+    fontSize: 30,
+    marginTop: 40,
+  },
+  Text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 20,
+    marginBottom: 6,
+  },
+  input: {
+    height: 40,
+    width: 370,
+    borderRadius: 50,
+    borderWidth: 0,
+    padding: 10,
+    backgroundColor: "#D9D9D9",
+    fontSize: 15,
+    opacity: 0.6,
+  },
+  roundButton1: {
+    width: 100,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 45,
+    borderRadius: 100,
+    backgroundColor: "#D9D9D9",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
+
+export default AddActivity;
